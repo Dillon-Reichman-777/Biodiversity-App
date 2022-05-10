38498df2f9f1b99edfd05c018ff5d75233ea52c8
Dillon <82934058+Dillon-Reichman-777@users.noreply.github.com>
2022-05-10 20:10:07 +0200

    Update test_cleaned_data.R

--- unit_tests/test_cleaned_data.R
+++ unit_tests/test_cleaned_data.R
@@ -10,7 +10,7 @@ test_that("Location vec has same dimention as the number of rows of poland_speci
 })
 
 test_that("We have no missing values",{
-  expect_equal(cleaned_data(poland_species_data)[[4]],5)
+  expect_equal(cleaned_data(poland_species_data)[[4]],0)
 })
 
 test_that("Location vec does not have char '-' ",{
